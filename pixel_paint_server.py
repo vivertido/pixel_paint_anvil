e485ec4046cd276a59b12891ba61d4421237e5af
Code Next <61989260+CodeNextAdmin@users.noreply.github.com>
2021-03-18 10:55:31 -0700

    removed spaces

--- pixel_paint_server.py
+++ pixel_paint_server.py
@@ -3,21 +3,18 @@ from sense_hat import SenseHat
 from time import sleep
 import datetime
 
-
 sense = SenseHat()
 sense.clear()
 
 @anvil.server.callable
 def clear_sense_hat():
-
+    
     sense.clear()
-
     pass
 
 @anvil.server.callable
 def save_pixels(pixels):
-    
-    
+      
     sense.clear()
     sense.show_message("saving")
     sleep(2)
@@ -27,23 +24,20 @@ def save_pixels(pixels):
     with open("my_designs.txt", 'a+') as f:
         ct = datetime.datetime.now() 
         f.write(""+str(ct))
-        f.write("\n\n")
-        
+        f.write("\n\n")       
         current_image = sense.get_pixels()
         f.write(str(current_image) + "\n\n")
         f.write("------------------------------------------------------------------------------" + "\n")
+        
     return "success"
 
 @anvil.server.callable
 def paint_pixel(data):
    
-    
     color = data["color"]
     x = data["coord"][0]
-    y = data["coord"][1]
-   
+    y = data["coord"][1]  
     sense.set_pixel(x, y, color)
-
     pass
 
 anvil.server.connect("<YOUR KEY HERE>")
